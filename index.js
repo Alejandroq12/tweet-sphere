7ea75d5fe315b22c435bce51a5d2c711811a21a3
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-02 14:44:22 -0600

    Add event listener to the button

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import { tweetsData } from './modules/data.js';
+const tweetInput = document.getElementById('tweet-input');
+const tweetBtn = document.getElementById('tweet-btn');
 
-console.log(tweetsData);
+tweetBtn.addEventListener('click', function () {
+  console.log(tweetInput.value)
+});
