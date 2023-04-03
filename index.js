1714b4d1de9836cac7635134ec9a06d121879407
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-02 18:04:58 -0600

    Add for of loop to iterate over the characters

--- index.js
+++ index.js
@@ -3,5 +3,28 @@ const tweetInput = document.getElementById('tweet-input');
 const tweetBtn = document.getElementById('tweet-btn');
 
 tweetBtn.addEventListener('click', function () {
-  console.log(tweetInput.value)
+  console.log(tweetInput.value);
 });
+
+function getFeedHtml() {
+  let feedHtml = ``;
+  for (let tweet of tweetsData) {
+    feedHtml += `<div class='tweet'>
+        <div class='tweet-inner'>
+          <img src='${tweet.profilePic}' class="profile-pic" />
+          <div>
+            <p class='handle'>${tweet.handle}</p>
+            <p class='tweet-text'>${tweet.tweetText}</p>
+            <div class='tweet-details'>
+              <span class='tweet-detail'>${tweet.replies.lenght}</span>
+              <span class='tweet-detail'>${tweet.likes}</span>
+              <span class='tweet-detail'>${tweet.retweets}</span>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+  return feedHtml;
+}
+
+getFeedHtml();
